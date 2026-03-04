6b516fac033d08ff7a042411cea25cd01a2b21bc
Johann Sebastian Schicho <sebastian-schicho@t-online.de>
2026-03-04 14:41:59 +0100

    fix compression with drag and drop in nodelink

--- src/js/nodelinkrender.js
+++ src/js/nodelinkrender.js
@@ -135,18 +135,25 @@ export class NodeLinkRenderer {
         }
 
         function drag(simulation) {
+            function precheck(event) {
+                return event.subject.get_state() !== State["Fully Compressed"];
+            }
+
             function dragstarted(event) {
+                if (!precheck(event)) return;
                 if (!event.active) simulation.alphaTarget(0.3).restart();
                 event.subject.fx = event.subject.x;
                 event.subject.fy = event.subject.y;
             }
 
             function dragged(event) {
+                if (!precheck(event)) return;
                 event.subject.fx = event.x;
                 event.subject.fy = event.y;
             }
 
             function dragended(event) {
+                if (!precheck(event)) return;
                 if (!event.active) simulation.alphaTarget(0);
                 event.subject.fx = null;
                 event.subject.fy = null;
